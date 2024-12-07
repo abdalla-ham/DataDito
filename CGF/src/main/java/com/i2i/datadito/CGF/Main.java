972680322fb29711341ad35518edce9b9d5dc655
Mehonal <116497904+mehonal@users.noreply.github.com>
2024-12-08 02:31:02 +0300

    CGF - refactored project to use com.i2i.datadito.CGF rather than com.datadito

--- CGF/src/main/java/com/i2i/datadito/CGF/Main.java
+++ CGF/src/main/java/com/i2i/datadito/CGF/Main.java
@@ -1,4 +1,4 @@
-package com.datadito;
+package com.i2i.datadito.CGF;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
