1caf19aae08b8068ff4d9f9e16b9f824137f87be
Mehonal <116497904+mehonal@users.noreply.github.com>
2024-12-08 19:32:35 +0300

    CGF - Added Main CGF function class

--- CGF/src/main/java/com/i2i/datadito/CGF/Main.java
+++ CGF/src/main/java/com/i2i/datadito/CGF/Main.java
@@ -1,17 +1,22 @@
 package com.i2i.datadito.CGF;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.i2i.datadito.kafka.MessageConsumer;
+import com.i2i.datadito.kafka.message.UsageRecordMessage;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main( String[] args ) {
+        MessageConsumer<UsageRecordMessage> subscriber = new MessageConsumer<>();
+        subscriber.createUsageRecordMessageConsumer();
+
+        // infinite loop listening to events from Kafka, writing to Oracle if found
+        while (true){
+            ConsumerRecords<String, UsageRecordMessage> consumerRecords = subscriber.poll();
+            for(ConsumerRecord<String, UsageRecordMessage> record: consumerRecords){
+                UsageRecordMessage usageRecord = record.value();
+                OracleCRUD.callInsertProcedure(usageRecord);
+            }
         }
     }
 }
