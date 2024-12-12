256fd95dca6d676b559bc46c3a13051d7f5c4311
Imadeddine Belkat <91804004+imadbelkat1@users.noreply.github.com>
2024-12-12 11:34:31 +0300

    .

---
+++ Kafka/src/main/java/com/i2i/datadito/kafka/seralizer/UsageRecordMessageSer.java
@@ -0,0 +1,10 @@
+package com.i2i.datadito.kafka.seralizer;
+
+import com.i2i.datadito.kafka.message.UsageRecordMessage;
+
+public class UsageRecordMessageSer extends GenericMessageSer<UsageRecordMessage> {
+
+    public UsageRecordMessageSer() {
+
+    }
+}
